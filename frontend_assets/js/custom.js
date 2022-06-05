5331a481492eb03b4d2564add377defc013ea2b8
Md. Jafrul Hasan Rasel <73773576+jhrasel@users.noreply.github.com>
2022-06-05 17:04:41 +0600

    Client Part Done

--- frontend_assets/js/custom.js
+++ frontend_assets/js/custom.js
@@ -1,10 +1,15 @@
-$(function() {
+// TopCompanies_item
+$('.owl-carousel.TopCompanies_slider').owlCarousel({
+    items:1,
+    loop:true,
+    nav:false,
+    dots:true,
+})
 
-
-
-
-
-
-
-
-});
+// 
+$('.owl-carousel.client_slider').owlCarousel({
+    items:1,
+    loop:true,
+    nav:true,
+    dots:false,
+})
